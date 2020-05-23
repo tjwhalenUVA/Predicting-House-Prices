54356a0e5734449fe95846b0469a4b19f8e85ead
Jake Whalen <tjw4ra@virginia.edu>
2020-05-23 11:30:06 -0400

    moved load_data to helper module

--- model_a.py
+++ model_a.py
@@ -8,16 +8,9 @@ Created on Thu Jan 16 20:54:21 2020
 import pandas as pd
 import os
 
-#%% Load the Data
-DATA_PATH = os.path.join(os.getcwd(), 'input')
+import helper_functions
 
-def load_housing_data(data_path = DATA_PATH, read_test=True):
-    train = pd.read_csv(os.path.join(data_path, 'train.csv'))
-    if read_test:
-        test = pd.read_csv(os.path.join(data_path, 'test.csv'))
-    return(train, test)
-
-train, test = load_housing_data()
+train, test = train, test = helper_functions.load_housing_data()
 
 #%% Inspect the Columns
 "Target is Sale Price"
